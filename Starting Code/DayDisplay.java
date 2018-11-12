48cbf6cb7ecd41944a86b7bd305e41c61575eb6b
Unknown <monika.silvia.s@hotmail.ca>
2018-11-12 11:46:56 -0800

    Notes and Starting Code

---
+++ Starting Code/DayDisplay.java
@@ -0,0 +1,29 @@
+
+/**
+ * DayDisplay class definition.
+ *
+ * @author Mike Mulder
+ * @version 1.0
+ */
+public class DayDisplay
+{
+    // Constants
+    public static final int NUMBER_OF_DAYS_IN_WEEK = 7;
+    public static final int SUNDAY                 = 0;
+    public static final int MONDAY                 = 1;
+    public static final int TUESDAY                = 2;
+    public static final int WEDNESDAY              = 3;
+    public static final int THURSDAY               = 4;
+    public static final int FRIDAY                 = 5;
+    public static final int SATURDAY               = 6;
+
+    public static final String SUNDAY_STRING       = "Sunday";
+    public static final String MONDAY_STRING       = "Monday";
+    public static final String TUESDAY_STRING      = "Tuesday";
+    public static final String WEDNESDAY_STRING    = "Wednesday";
+    public static final String THURSDAY_STRING     = "Thursday";
+    public static final String FRIDAY_STRING       = "Friday";
+    public static final String SATURDAY_STRING     = "Saturday";
+
+
+}
